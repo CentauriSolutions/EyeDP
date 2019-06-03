ed35e0fc013b6d8f0b2199bd714bd74d113b61eb
Chris MacNaughton <chris@centaurisolutions.nl>
2019-06-03 09:40:31 +0200

    Add some specs to assert basic auth works as expected

--- app/controllers/basic_auth_controller.rb
+++ app/controllers/basic_auth_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class BasicAuthController < ApplicationController
+  skip_before_action :authenticate_user!
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
   def create
---
+++ spec/controllers/basic_auth_controller_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BasicAuthController, type: :controller do
+  let(:user) { User.create!(email: 'test@localhost', password: 'test1234') }
+
+  describe 'unauthenticated_user' do
+    it 'forbids non-authenticated user' do
+      get :create, params: { permission_name: 'use.test_app' }
+      expect(response.status).to eq(403)
+    end
+  end
+
+  describe 'authenticated_user' do
+    let(:permission) { Permission.create!(name: 'use.test_app') }
+    let(:group) { Group.create!(name: 'my_group', permissions: [permission]) }
+
+    it 'allows authenticated role with group' do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user.groups << group
+      sign_in user
+      get :create, params: { permission_name: 'use.test_app' }
+      expect(response.status).to eq(200)
+    end
+    it 'forbids authenticated role without group' do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      sign_in user
+      get :create, params: { permission_name: 'use.test_app' }
+      expect(response.status).to eq(403)
+    end
+  end
+end
