46cc453fb7cd929e9549742ae464d1c38e2005c2
Chris MacNaughton <chris@centaurisolutions.nl>
2019-06-12 12:13:05 +0200

    Track user logins to SAML providers
    [Finished #166633677]

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -6,17 +6,23 @@ class SamlIdpController < SamlIdp::IdpController
   # rubocop:enable Rails/LexicallyScopedActionFilter
 
   # override create and make sure to set both "GET" and "POST" requests to /saml/auth to #create
+  # rubocop:disable Metrics/MethodLength
   def create
     # binding.pry
     if user_signed_in?
       @saml_response = idp_make_saml_response(current_user)
       render template: 'saml_idp/idp/saml_post', layout: false
+      Login.create(
+        user: current_user,
+        service_provider: SamlServiceProvider.find_by(issuer_or_entity_id: @saml_request.issuer)
+      )
       nil
     else
       # it shouldn't be possible to get here, but lets render 403 just in case
       render status: :forbidden
     end
   end
+  # rubocop:enable Metrics/MethodLength
 
   # NOT USED -- def idp_authenticate(email, password) -- NOT USED
 
