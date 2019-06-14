6a195a7bf6e6442c48c5f82886b7ea87f5af5fa7
Chris MacNaughton <chris@centaurisolutions.nl>
2019-06-14 21:25:17 +0200

    working on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,8 +34,8 @@ set :puma_init_active_record, false # Change to true if using ActiveRecord
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml config/master.key}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
