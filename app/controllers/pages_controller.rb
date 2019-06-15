f28e940793994bf0b9611d1cec5ab57178a44ae0
Chris MacNaughton <chris@centaurisolutions.nl>
2019-06-15 11:46:04 +0200

    fix callback order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
   before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :home
 
   def home; end
 
