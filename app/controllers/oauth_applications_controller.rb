3ef970a5ba2cb883dbf9eb403111b3ecb8afbb40
Chris MacNaughton <chris.macnaughton@canonical.com>
2020-03-18 20:47:59 +0100

    move to new Server#client option

--- app/controllers/oauth_applications_controller.rb
+++ app/controllers/oauth_applications_controller.rb
@@ -3,6 +3,6 @@
 class OauthApplicationsController < Doorkeeper::AuthorizationsController
   def create
     super
-    Login.create(user: current_user, service_provider: server.client_via_uid.application)
+    Login.create(user: current_user, service_provider: server.client.application)
   end
 end
