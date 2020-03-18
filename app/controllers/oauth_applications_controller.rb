7e28cc8338ada083a478f9bf6feb0c244ddc6106
Chris MacNaughton <chris.macnaughton@canonical.com>
2020-03-18 21:12:17 +0100

    access Application directly

--- app/controllers/oauth_applications_controller.rb
+++ app/controllers/oauth_applications_controller.rb
@@ -3,6 +3,6 @@
 class OauthApplicationsController < Doorkeeper::AuthorizationsController
   def create
     super
-    Login.create(user: current_user, service_provider: server.client.application)
+    Login.create(user: current_user, service_provider: Application.find_by(uid: params[:client_id]))
   end
 end
