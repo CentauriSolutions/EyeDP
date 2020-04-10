cec5fad856a109904d255a31c32b7d86e0a89203
Chris MacNaughton <chris.macnaughton@canonical.com>
2020-04-10 20:35:43 +0200

    Add spec for Permisison

---
+++ spec/models/permission_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Permission, type: :model do
+  let(:permission) { Permission.create!(name: 'test permission') }
+
+  it 'uses name as to_s' do
+    expect(permission.to_s).to eq 'test permission'
+  end
+end
