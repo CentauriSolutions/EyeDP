1246b8939649a8f94b4d48f5a571f94e39f8df45
Chris MacNaughton <chris@centauri.solutions>
2020-04-17 20:18:18 +0200

    Get the oauth application specs working
    [Finished #172384820]

--- spec/controllers/oauth_applications_controller_spec.rb
+++ spec/controllers/oauth_applications_controller_spec.rb
@@ -4,50 +4,47 @@ require 'rails_helper'
 
 RSpec.describe OauthApplicationsController, type: :controller do
   let(:user) { User.create!(username: 'example', email: 'test@localhost', password: 'test1234') }
-  let(:app) { Application.create!(uid: 'test', internal: true ) }
-
-  describe 'Existing Login' do
-    it 'records the login'#  do
-    #   @request.env['devise.mapping'] = Devise.mappings[:user]
-    #   sign_in user
-    #   get :new, params: { client_id: 'test' }
-    #   expect(response.status).to eq(200)
-    #   expect(Login.count).to eq 1
-    # end
+  let(:app) { Application.create!(uid: 'test', internal: true, redirect_uri: 'https://example.com', name: 'test' ) }
+  let(:params) do
+    {
+      response_type: "code",
+      client_id: app.uid,
+      redirect_uri: app.redirect_uri,
+      state: 'state'
+    }
   end
 
-  describe 'New login' do
-    it 'records the login'#  do
-    #   @request.env['devise.mapping'] = Devise.mappings[:user]
-    #   sign_in user
-    #   get :create, params: { client_id: 'test' }
-    #   expect(response.status).to eq(200)
-    #   expect(Login.count).to eq 1
-    # end
+  context "signed in user" do
+    before do
+      sign_in(user)
+    end
+
+
+    describe 'Existing Login' do
+      it 'records the login'  do
+        # @request.env['devise.mapping'] = Devise.mappings[:user]
+        get :create, params: params
+        expect(response.status).to eq(302)
+        expect(Login.count).to eq 1
+      end
+    end
+
+    describe 'New login' do
+      it 'records the login'  do
+        # @request.env['devise.mapping'] = Devise.mappings[:user]
+        get :create, params: params
+        expect(response.status).to eq(302)
+        expect(Login.count).to eq 1
+      end
+    end
   end
-  # describe 'unauthenticated_user' do
-  #   it 'forbids non-authenticated user' do
-  #     get :create, params: { permission_name: 'use.test_app' }
-  #     expect(response.status).to eq(401)
-  #   end
-  # end
 
-  # describe 'authenticated_user' do
-  #   let(:permission) { Permission.create!(name: 'use.test_app') }
-  #   let(:group) { Group.create!(name: 'my_group', permissions: [permission]) }
+  context "signed out user" do
+    it 'returns 301 code and redirects to login' do
+      get :new
+      expect(response.status).to eq(302)
+      expect(response.headers['Location']).to eq('http://test.host/users/sign_in')
 
-  #   it 'allows authenticated role with group' do
-  #     @request.env['devise.mapping'] = Devise.mappings[:user]
-  #     user.groups << group
-  #     sign_in user
-  #     get :create, params: { permission_name: 'use.test_app' }
-  #     expect(response.status).to eq(200)
-  #   end
-  #   it 'forbids authenticated role without group' do
-  #     @request.env['devise.mapping'] = Devise.mappings[:user]
-  #     sign_in user
-  #     get :create, params: { permission_name: 'use.test_app' }
-  #     expect(response.status).to eq(401)
-  #   end
-  # end
+    end
+  end
 end
