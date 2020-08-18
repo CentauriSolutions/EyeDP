3cae555a17d2fb916084fc1079ba7df2321b021c
Chris MacNaughton <chris@centaurisolutions.nl>
2020-08-18 11:33:41 +0200

    Fix attribute sanitization with user creation
    Closes #58

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
