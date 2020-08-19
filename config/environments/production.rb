a26e445a82496f1dab4d26243f2aa3442a84f729
Chris MacNaughton <chris@centaurisolutions.nl>
2020-08-19 15:09:34 +0200

    Allow EMAIL setting to be configured via the environment (#68)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.force_ssl = true unless ENV['DISABLE_SSL'] == 'true'
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = ENV['LOG_LEVEL'] || :warning
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
