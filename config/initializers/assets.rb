88d19d67fc1eeb0823d8f1fe6b54017234d7f9eb
Chris MacNaughton <chmacnaughton@gmail.com>
2020-08-25 08:18:46 +0200

    An admin can configure a custom logo for the navigation bar
    Closes #79

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,10 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.paths << Rails.root.join('public/images')
+
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
