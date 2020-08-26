89a40c5a31375123307927d63273e062bf488cc9
Chris MacNaughton <chris@centaurisolutions.nl>
2020-08-26 15:47:46 +0200

    Add username, email, and group names to logged in template (#84)
    Closes #83

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,19 @@ class PagesController < ApplicationController
     @template = Liquid::Template.parse(Setting.registered_home_template)
     @logins = current_user.logins.page(params[:page] || 1).includes(:service_provider).order(created_at: :desc)
   end
+
+  def template_variables
+    if current_user
+      {
+        'user' => {
+          'username' => current_user.username,
+          'email' => current_user.email,
+        },
+        'groups' => current_user.groups.pluck(:name)
+      }
+    else
+      {}
+    end
+  end
+  helper_method :template_variables
 end
