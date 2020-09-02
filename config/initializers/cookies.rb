e43c3579cc109821a666b046cf8a7b3a2b8161d7
Chris MacNaughton <chris@centaurisolutions.nl>
2020-09-02 14:20:31 +0200

    Ensure the session cookie is secure only in produciton (#85)

--- config/initializers/cookies.rb
+++ config/initializers/cookies.rb
@@ -2,4 +2,5 @@
 EyedP::Application.config.session_store :cookie_store,
     key: '_eyed_p_session',
     domain: ENV['SSO_DOMAIN'] || :all,
-    tld_length: 2
+    tld_length: 2,
+    secure: Rails.env.production?
