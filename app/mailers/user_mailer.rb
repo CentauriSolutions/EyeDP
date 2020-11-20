24c33548c000c413aa5e541f30c2c99f899694b6
Chris MacNaughton <chmacnaughton@gmail.com>
2020-11-20 10:35:07 +0100

    Remove translations as they are broken right now

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,20 +7,20 @@ class UserMailer < ApplicationMailer
     @user = user
     @group = group
     mail(to: @user.email,
-         subject: I18n.t("Welcome to #{@group.name}"))
+         subject: "Welcome to #{@group.name}")
   end
 
   def force_reset_password_email(user, token)
     @user = user
     @token = token
     mail(to: @user.email,
-         subject: I18n.t('Password Changed'))
+         subject: 'Password Changed')
   end
 
   def admin_welcome_email(user, token)
     @user = user
     @token = token
     mail(to: @user.email,
-         subject: I18n.t('Your account has been created'))
+         subject: 'Your account has been created')
   end
 end
