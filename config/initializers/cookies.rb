5e342095ba9c155fb59074964e5f56e9ebd2784f
Chris MacNaughton <chris@centauri.solutions>
2020-12-01 11:08:23 +0100

    allow disabling SSL in production (#161)

--- config/initializers/cookies.rb
+++ config/initializers/cookies.rb
@@ -4,4 +4,4 @@ EyedP::Application.config.session_store :cookie_store,
                                         key: '_eyed_p_session',
                                         domain: ENV['SSO_DOMAIN'] || :all,
                                         tld_length: 2,
-                                        secure: Rails.env.production?
+                                        secure: Rails.env.production? && !ENV['DISABLE_SSL']
