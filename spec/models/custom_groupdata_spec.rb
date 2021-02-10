b764db451492cf37e5f462f54add79c66d945783
Chris MacNaughton <chmacnaughton@gmail.com>
2021-02-10 08:31:19 +0100

    Add rspec tests for auditable

--- spec/models/custom_groupdata_spec.rb
+++ spec/models/custom_groupdata_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe CustomGroupdatum, type: :model do
   let(:custom_type) { CustomGroupDataType.create(name: 'alias', custom_type: 'string') }
   let(:group) { Group.create!(name: 'root') }
 
+  it { should be_audited }
+
   it 'A user can have a custom user data' do
     data = CustomGroupdatum.create!(group: group, custom_group_data_type: custom_type, value: 'ahoy')
     expect(data.name).to eq('alias')
---
+++ spec/models/setting_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Setting, type: :model do
+
+  it { should be_audited }
+end
