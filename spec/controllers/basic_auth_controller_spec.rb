4693e80f99a890bd5ec94ec6820a4ddd6e36ad85
Chris MacNaughton <chris@centauri.solutions>
2021-03-23 07:58:12 +0100

    Do not redirect when the user session is expired (#206)
    The addition of `:timeoutable` in the `User` model
    introduces this subtle bug because of how Warden
    handles timeout compared to not logged in.

--- spec/controllers/basic_auth_controller_spec.rb
+++ spec/controllers/basic_auth_controller_spec.rb
@@ -27,6 +27,17 @@ RSpec.describe BasicAuthController, type: :controller do
       expect(user.last_activity_at).not_to eq(start)
     end
 
+    it 'forbids an authenticated, timed out user' do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user.groups << group
+      sign_in user
+      warden.session('user')['last_request_at'] = 1.day.ago
+      # user.last_acccess = 1.day.ago
+      get :create, params: { permission_name: 'use.test_app' }
+
+      expect(response.status).to eq(401)
+    end
+
     it 'forbids authenticated role without required two factor' do
       group.update({ requires_2fa: true })
       @request.env['devise.mapping'] = Devise.mappings[:user]
