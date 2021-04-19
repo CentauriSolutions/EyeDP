2a69dd595f3c7f53e2ce0dcda495e571faae5198
Chris MacNaughton <chmacnaughton@gmail.com>
2021-04-19 15:12:30 +0200

    Skip audit logs for migration
    Closes #215

--- db/migrate/20201221120328_add_admin_field_to_groups.rb
+++ db/migrate/20201221120328_add_admin_field_to_groups.rb
@@ -2,7 +2,9 @@ class AddAdminFieldToGroups < ActiveRecord::Migration[6.0]
   def up
     transaction do
       add_column :groups, :admin, :boolean, default: false
-      Group.where(name: 'administrators').update({ admin: true })
+      Group.without_auditing do
+        Group.where(name: 'administrators').update({ admin: true })
+      end
     end
   end
 
