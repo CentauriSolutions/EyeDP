14711e05549bf8908656f938f7578068766d0b2a
Chris MacNaughton <chmacnaughton@gmail.com>
2021-04-23 14:59:23 +0200

    Manually update the warden session last_activity_at
    Closes #221

--- spec/controllers/basic_auth_controller_spec.rb
+++ spec/controllers/basic_auth_controller_spec.rb
@@ -38,6 +38,26 @@ RSpec.describe BasicAuthController, type: :controller do
       expect(response.status).to eq(401)
     end
 
+    it 'it updates last_request_at on use' do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user.groups << group
+      sign_in user
+      start = 5.minutes.ago.to_i
+      warden.session('user')['last_request_at'] = start
+      get :create, params: { permission_name: 'use.test_app' }
+      expect(response.status).to eq(200)
+      expect(warden.session('user')['last_request_at']).not_to eq(start)
+      # Warden is apparently doing something super weird with deciding
+      # to update or not the last_request_at field. The check above passes,
+      # even though it matches this one perfectly. This test is to comfirm
+      # that no regressions are introduced that cause timeouts to happen
+      # while a user regularly hits the auth backend.
+      warden.session('user')['last_request_at'] = start
+      get :create, params: { permission_name: 'use.test_app' }
+      expect(response.status).to eq(200)
+      expect(warden.session('user')['last_request_at']).not_to eq(start)
+    end
+
     it 'forbids authenticated role without required two factor' do
       group.update({ requires_2fa: true })
       @request.env['devise.mapping'] = Devise.mappings[:user]
