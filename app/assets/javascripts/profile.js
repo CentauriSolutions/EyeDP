3b42037a0f1e6093abe33beb5522acf3887a2843
Chris MacNaughton <chris@centauri.solutions>
2021-04-24 17:48:09 +0200

    Add a UI element to remove elements from the list

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,8 +1,14 @@
-function add_field(event, ) {
-	var button = $(event);
+function add_field(item) {
+	var button = $(item);
 	var name = button.data('name');
 	var target = button.data('target');
 	var text_field = $('<input class="form-control" type="text" name="' + name + '[]" value="" placeholder="Add Value">');
 	var container = $('#' + target);
 	container.append(text_field);
+}
+
+function remove_element(item) {
+	var button = $(item);
+	var target = button.data('target');
+	$('#'+target).remove()
 }
