51f20d244a30c2fdcea9be3dc985978911f4e4c8
Chris MacNaughton <chmacnaughton@gmail.com>
2021-05-06 11:48:21 +0200

    An admin can enable and disable :timeoutable
    When setting the session_timeout_in value to nil, Devise
    stops timing out the user. This change also updates the
    default setting to disable the feature, but has been tested
    to ensure that, if an admin has customised this setting,
    their customisation will still be in place.
    
    Closes #237

--- spec/controllers/basic_auth_controller_spec.rb
+++ spec/controllers/basic_auth_controller_spec.rb
@@ -16,46 +16,83 @@ RSpec.describe BasicAuthController, type: :controller do
     let(:permission) { Permission.create!(name: 'use.test_app') }
     let(:group) { Group.create!(name: 'my_group', permissions: [permission]) }
 
-    it 'allows authenticated role with group' do
-      start = user.last_activity_at
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      user.groups << group
-      sign_in user
-      get :create, params: { permission_name: 'use.test_app' }
-      expect(response.status).to eq(200)
-      user.reload
-      expect(user.last_activity_at).not_to eq(start)
+    context 'with session timeouts disabled (default)' do
+      before do
+        Setting.session_timeout_in = nil
+      end
+
+      it 'allows authenticated role with group' do
+        start = user.last_activity_at
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+        user.groups << group
+        sign_in user
+        get :create, params: { permission_name: 'use.test_app' }
+        expect(response.status).to eq(200)
+        user.reload
+        expect(user.last_activity_at).not_to eq(start)
+      end
+
+      it 'allows an authenticated, old user session' do
+        start = user.last_activity_at
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+        user.groups << group
+        sign_in user
+        warden.session('user')['last_request_at'] = 7
+        # user.last_acccess = 1.day.ago
+        get :create, params: { permission_name: 'use.test_app' }
+
+        expect(response.status).to eq(200)
+        user.reload
+        expect(user.last_activity_at).not_to eq(start)
+      end
     end
 
-    it 'forbids an authenticated, timed out user' do
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      user.groups << group
-      sign_in user
-      warden.session('user')['last_request_at'] = 7
-      # user.last_acccess = 1.day.ago
-      get :create, params: { permission_name: 'use.test_app' }
+    context 'with session timeouts enabled' do
+      before do
+        Setting.session_timeout_in = 1.hour
+      end
 
-      expect(response.status).to eq(401)
-    end
+      it 'allows authenticated role with group' do
+        start = user.last_activity_at
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+        user.groups << group
+        sign_in user
+        get :create, params: { permission_name: 'use.test_app' }
+        expect(response.status).to eq(200)
+        user.reload
+        expect(user.last_activity_at).not_to eq(start)
+      end
 
-    it 'it updates last_request_at on use' do
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      user.groups << group
-      sign_in user
-      start = 5.minutes.ago.to_i
-      warden.session('user')['last_request_at'] = start
-      get :create, params: { permission_name: 'use.test_app' }
-      expect(response.status).to eq(200)
-      expect(warden.session('user')['last_request_at']).not_to eq(start)
-      # Warden is apparently doing something super weird with deciding
-      # to update or not the last_request_at field. The check above passes,
-      # even though it matches this one perfectly. This test is to comfirm
-      # that no regressions are introduced that cause timeouts to happen
-      # while a user regularly hits the auth backend.
-      warden.session('user')['last_request_at'] = start
-      get :create, params: { permission_name: 'use.test_app' }
-      expect(response.status).to eq(200)
-      expect(warden.session('user')['last_request_at']).not_to eq(start)
+      it 'forbids an authenticated, timed out user' do
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+        user.groups << group
+        sign_in user
+        warden.session('user')['last_request_at'] = 7
+        # user.last_acccess = 1.day.ago
+        get :create, params: { permission_name: 'use.test_app' }
+
+        expect(response.status).to eq(401)
+      end
+
+      it 'it updates last_request_at on use' do
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+        user.groups << group
+        sign_in user
+        start = 5.minutes.ago.to_i
+        warden.session('user')['last_request_at'] = start
+        get :create, params: { permission_name: 'use.test_app' }
+        expect(response.status).to eq(200)
+        expect(warden.session('user')['last_request_at']).not_to eq(start)
+        # Warden is apparently doing something super weird with deciding
+        # to update or not the last_request_at field. The check above passes,
+        # even though it matches this one perfectly. This test is to comfirm
+        # that no regressions are introduced that cause timeouts to happen
+        # while a user regularly hits the auth backend.
+        warden.session('user')['last_request_at'] = start
+        get :create, params: { permission_name: 'use.test_app' }
+        expect(response.status).to eq(200)
+        expect(warden.session('user')['last_request_at']).not_to eq(start)
+      end
     end
 
     it 'forbids authenticated role without required two factor' do
