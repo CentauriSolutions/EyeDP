81f02ad59edcfbaeae96f390bb238a96ecaeab00
Chris MacNaughton <chris@cantauri.solutions>
2021-09-24 14:38:38 +0200

    Add more summarizing information to the dashboard.
    Closes #279

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,34 @@
 # frozen_string_literal: true
 
 class Admin::DashboardController < AdminController
-  def index; end
+  def index
+    @logins = Login.order(created_at: :desc).limit(50).includes(:service_provider, :user)
+  end
 
   def jobs; end
+
+  private
+
+  def logins_by_app # rubocop:disable Metrics/AbcSize
+    @logins_by_app ||= begin
+      logins = Login.group(%i[service_provider_type service_provider_id])
+                    .where('created_at > ?', 7.days.ago).count
+      logins = logins.map do |deets, count|
+        app = deets[0].constantize.find(deets[1])
+        app = Application.find(app.id) if app.is_a? Doorkeeper::Application
+        [app, count]
+      end
+      logins.sort_by { |login| - login[1] }.take(10)
+    end
+  end
+  helper_method :logins_by_app
+
+  def logins_by_user
+    @logins_by_user ||= Login.group(:user)
+                             .where('created_at > ?', 7.days.ago)
+                             .count
+                             .sort_by { |login| - login[1] }
+                             .take(10)
+  end
+  helper_method :logins_by_user
 end
