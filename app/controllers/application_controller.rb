125d0026a519dd173146120a8d852137c370ea8b
Chris MacNaughton <chris@centauri.solutions>
2021-11-20 15:40:40 +0100

    Fix changing and persisting localisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,8 @@ class ApplicationController < ActionController::Base
   # before the location can be stored.
 
   def set_locale
-    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
-  end
-
-  def default_url_options
-    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+    I18n.locale = params.fetch(:locale, session[:locale], I18n.default_locale).to_sym
+    session[:locale] = I18n.locale
   end
 
   def after_sign_in_path_for(resource)
