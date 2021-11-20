5c9144ce9e06ff6b4fc1937c5d3ddf3190ad4bd7
Chris MacNaughton <chris@centauri.solutions>
2021-11-20 15:40:40 +0100

    Fix changing and persisting localisation

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 I18n.default_locale = :en
-I18n.available_locales = %i[en fr]
+I18n.available_locales = %i[en nl]
 I18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
