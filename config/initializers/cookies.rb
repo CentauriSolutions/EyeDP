4e8d466e77846417d1105a1c03e942fab5b65a29
Chris MacNaughton <chris@centauri.solutions>
2022-06-02 14:05:30 +0200

    Add session expiration into Redis
    In addition to regular session timeouts, remove the
    keys from Redis after 30 days of inactivity.

--- config/initializers/cookies.rb
+++ config/initializers/cookies.rb
@@ -2,6 +2,7 @@
 
 url = ENV['REDIS_URL'] || 'redis://localhost:6379'
 EyedP::Application.config.session_store :redis_store,
+                                        expire_after: 30.days,
                                         key: '_eyed_p_session',
                                         domain: ENV['SSO_DOMAIN'] || :all,
                                         tld_length: 2,
