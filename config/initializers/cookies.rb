f5281fc4afcb89ac3b3ae54abfc958c9d7dad0fc
Chris MacNaughton <chris@centauri.solutions>
2022-07-24 20:14:00 -0500

    Tests should not depend on Redis connectivity

--- config/initializers/cookies.rb
+++ config/initializers/cookies.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-url = ENV['REDIS_URL'] || 'redis://localhost:6379'
-EyedP::Application.config.session_store :redis_store,
-                                        expire_after: 30.days,
-                                        key: '_eyed_p_session',
-                                        domain: ENV['SSO_DOMAIN'] || :all,
-                                        tld_length: 2,
-                                        secure: Rails.env.production? && !ENV['DISABLE_SSL'],
-                                        servers: [{
-                                          url: url,
-                                          serializer: JSON
-                                        }]
+unless Rails.env.test?
+  url = ENV['REDIS_URL'] || 'redis://localhost:6379'
+  EyedP::Application.config.session_store :redis_store,
+                                          expire_after: 30.days,
+                                          key: '_eyed_p_session',
+                                          domain: ENV['SSO_DOMAIN'] || :all,
+                                          tld_length: 2,
+                                          secure: Rails.env.production? && !ENV['DISABLE_SSL'],
+                                          servers: [{
+                                            url: url,
+                                            serializer: JSON
+                                          }]
+end
