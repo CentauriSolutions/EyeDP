b6659e0106747cf126fd20c8150ec9fd77672c2e
Chris MacNaughton <chris@centauri.solutions>
2023-11-05 10:27:05 -0500

    remove U2F tests as U2F isn't functionally exposed anymore

--- spec/controllers/sudo_rails/application_controller_spec.rb
+++ spec/controllers/sudo_rails/application_controller_spec.rb
@@ -46,56 +46,12 @@ RSpec.describe SudoRails::ApplicationController, type: :controller do
       user
     end
 
-    def authenticate_2fa_u2f(user_params)
-      post(:create, params: { user: user_params }, session: { otp_user_id: user.id })
-    end
-
-    def create_u2f_device(user, key_handle, public_key, certificate, attributes = {})
-      attrib = {
-        user: user,
-        name: 'Unnamed 1',
-        key_handle: key_handle,
-        public_key: public_key,
-        certificate: certificate,
-        counter: 0,
-        last_authenticated_at: Time.zone.now
-      }.update(attributes)
-      FidoUsf::FidoUsfDevice.create!(attrib)
-    end
-
-    def setup_u2f(controller)
-      setup_u2f_with_appid(controller.u2f_app_id)
-    end
-
-    def setup_u2f_with_appid(app_id)
-      device = U2F::FakeU2F.new(app_id)
-      key_handle = U2F.urlsafe_encode64(device.key_handle_raw)
-      certificate = Base64.strict_encode64(device.cert_raw)
-      public_key = device.origin_public_key_raw
-      { device: device, key_handle: key_handle, certificate: certificate, public_key: public_key }
-    end
-
     context 'via TOTP' do
       it 'redirects to target path with a valid sudo session' do
         post(:confirm, params: { target_path: @target_path, user: { otp_attempt: user.current_otp } })
         expect(SudoRails.valid_sudo_session?(session[:sudo_session])).to eq(true)
         expect(response).to redirect_to @target_path
         expect(flash[:alert]).to be nil
-      end
-    end
-    context 'via U2F device' do
-      it 'redirects to target path with a valid sudo session' do
-        token = setup_u2f(@controller)
-        create_u2f_device(user, token[:key_handle], token[:public_key], token[:certificate])
-        device_response = token[:device].sign_response(@controller.session[:user_u2f_challenge])
-        post :confirm, params: { user: { device_response: device_response }, target_path: @target_path }
-
-        expect(SudoRails.valid_sudo_session?(session[:sudo_session])).to eq(true)
-        expect(response).to redirect_to @target_path
-        expect(flash[:alert]).to be nil
-        # authenticate_2fa_u2f(remember_me: '1', login: user.username, device_response: device_response)
-        # expect(response.cookies['remember_user_token']).to be_nil
-        # expect(subject.current_user).to eq user
       end
     end
   end
